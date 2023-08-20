3abf01b24da40febb33ccc450fd68debd9d0e92e
AdvaitSan <124705956+AdvaitSan@users.noreply.github.com>
2023-08-20 16:04:32 +0530

    animated navbar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { useEffect, useState } from 'react';
 import Card from './card';
 import dhooni from './images/dhooni baba.png'
 import Search from './Search';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {motion} from 'framer-motion'
 
 function App() {
   const [products, setProducts] = useState([]);
@@ -30,12 +31,14 @@ function App() {
     else {
       setSkip(skip - 30)
       setNums(nums - 1)
+      getData();
     }
   }
 
   const NextClick = () => {
     setSkip(skip + 30);
     setNums(nums + 1);
+    getData();
   }
 
   useEffect(() => {
@@ -43,29 +46,29 @@ function App() {
     getData();
   }, [skip]);
 
-  useEffect(() => {
-    getData();
-  }, [api]);
-
   return (
     <Router>
       <div className='bg-gray-400 border-2 border-black'>
-        <div className='Navbar'>
+        <motion.div
+        initial={{y: -250}}
+        animate={{y: 0}}
+        transition={{delay:1}}
+         className='Navbar'>
           <div className="flex flex-wrap justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500 border-4 border-black">
             <div className='flex flex-wrap justify-center items-center bg-yellow-200 rounded-full my-3'>
               <div>
                 <img className="w-20 rounded-full ml-10 border-1 border-orange-700" src={dhooni} alt="Logo" />
               </div>
               <div>
-                <p className='text-4xl font-mono font-bold text-emerald-600 mt-1 mr-10'> Dhoonibaba.com</p>
+                <p className='text-4xl font-mono font-bold text-emerald-600 mt-1 ml-10 mr-10'> Dhoonibaba.com</p>
               </div>
             </div>
             <div>
-              <Link to="/search" className='bg-red-700 text-2xl text-white rounded-xl pl-3 pr-1 pb-1 ml-10 border-white border-2'>Category</Link>
-              <Link to="/" className='bg-red-700 text-2xl text-white rounded-xl px-5 pb-1 ml-10 border-white border-2'>Home</Link>
+              <a href="/search" className='bg-red-700 text-2xl text-white rounded-xl pl-3 pr-1 pb-1 ml-10 border-white border-2'>Category</a>
+              <a href="/" className='bg-red-700 text-2xl text-white rounded-xl px-5 pb-1 ml-10 border-white border-2'>Home</a>
             </div>
           </div>
-        </div>
+        </motion.div>
         <Switch>
           <Route exact path="/">
             <div className='cards'>
@@ -77,7 +80,11 @@ function App() {
                 ))}
               </div>
             </div>
-            <div className='Footer'>
+            <motion.div
+            initial={{opacity:0,x:0}}
+            animate={{opacity:1,x:-10}}
+            transition={{delay:2}}
+            className='Footer'>
               <div className='flex flex-wrap justify-center items-center'>
                 <button
                   className='bg-yellow-400 font-mono font-bold mx-4 my-8 text-5xl rounded-lg'
@@ -103,7 +110,7 @@ function App() {
                   Next
                 </button>
               </div>
-            </div>
+            </motion.div>
           </Route>
           <Route exact path="/search">
             <Search/>
